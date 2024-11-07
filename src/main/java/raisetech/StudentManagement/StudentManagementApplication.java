54b0f8b6fa557ad9c18fca90805ac687d9abb125
Kubota <l2303751284as@gmail.com>
2024-11-07 15:41:14 +0900

    第5回演習問題動作確認

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -3,18 +3,42 @@ package raisetech.StudentManagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+	private String name = "Kubota Shou";
+	private String age = "24";
+	private String name2 = "Satou Tarou";
+	private String age2 = "36";
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(){
-		return "Hello, world!";
+	@GetMapping("/studentInfo")
+	public String getStudentInfo(){
+		return name + " " + age + "歳" + name2 + " " + age2 + "歳";
+	}
+
+	@PostMapping("/studentInfo")
+	public void setStudentInfo(String name, String age, String name2, String age2){
+		this.name = name;
+		this.age = age;
+		this.name2 = name2;
+		this.age2 = age2;
+	}
+
+	@PostMapping("/studentName")
+	public void updateStudentName(String name){
+		this.name = name;
+	}
+
+	@PostMapping("/studentName2")
+	public void updateStudentName2(String name2){
+		this.name2 = name2;
 	}
 }
