077df329b8eee766262fc11e9eda6dbb942958bc
Kubota <l2303751284as@gmail.com>
2025-03-03 10:24:11 +0900

    スタンダード第21回演習再問題提出

--- src/main/java/raisetech/StudentManagement/domain/StudentDetail.java
+++ src/main/java/raisetech/StudentManagement/domain/StudentDetail.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import raisetech.StudentManagement.data.Student;
-import raisetech.StudentManagement.data.StudentsCourses;
+import raisetech.StudentManagement.data.StudentCourse;
 
 @Getter
 @Setter
@@ -14,7 +14,6 @@ import raisetech.StudentManagement.data.StudentsCourses;
 @AllArgsConstructor
 
 public class StudentDetail {
-
   private Student student;
-  private List<StudentsCourses> studentsCourses;
+  private List<StudentCourse> studentCourseList;
 }
--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 import raisetech.StudentManagement.data.Student;
-import raisetech.StudentManagement.data.StudentsCourses;
+import raisetech.StudentManagement.data.StudentCourse;
 
 /**
  * 受講生テーブルと受講生コース情報テーブルと紐づくRepositoryです。
@@ -20,7 +20,6 @@ public interface StudentRepository {
    *
    * @return　受講生一覧（全件）
    */
-  @Select("SELECT * FROM students")
   List<Student> search();
 
   /**
@@ -29,7 +28,6 @@ public interface StudentRepository {
    * @param id　受講生ID
    * @return　受講生
    */
-  @Select("SELECT * FROM students WHERE id = #{id}")
   Student searchStudent(String id);
 
   /**
@@ -38,7 +36,7 @@ public interface StudentRepository {
    * @return　受講生のコース情報（全件）
    */
   @Select("SELECT * FROM students_courses")
-  List<StudentsCourses> searchStudentsCoursesList();
+  List<StudentCourse> searchStudentCourseList();
 
   /**
    * 受講生IDに紐づく受講生コース情報を検索します。
@@ -47,25 +45,44 @@ public interface StudentRepository {
    * @return　受講生IDに紐づく受講生コース情報
    */
   @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
-  List<StudentsCourses> searchStudentsCourses(String studentId);
+  List<StudentCourse> searchStudentCourse(String studentId);
 
+  /**
+   * 受講生を新規登録します。IDに関しては自動採番を行う。
+   *
+   * @param student　受講生
+   */
   @Insert(
       "INSERT INTO students (name, kananame, nickname, email, area, age, sex, remark,is_deleted) "
           + "VALUES (#{name}, #{kanaName},  #{nickname}, #{email}, #{area}, #{age}, #{sex}, #{remark},false)")
-
   @Options(useGeneratedKeys = true, keyProperty = "id")
   void registerStudent(Student student);
 
+  /**
+   * 受講生コース情報を新規登録します。IDに関しては自動採番を行う。
+   *
+   * @param studentCourse　受講生コース情報
+   */
   @Insert("INSERT INTO students_courses(student_id, course_name, start_time, end_time)"
       + "VALUES(#{studentId},#{courseName},#{startTime},#{endTime})")
 
   @Options(useGeneratedKeys = true, keyProperty = "id")
-  void registerStudentsCourses(StudentsCourses studentsCourses);
+  void registerStudentCourse(StudentCourse studentCourse);
 
+  /**
+   * 受講生を更新します。
+   *
+   * @param student　受講生
+   */
   @Update("UPDATE students SET name = #{name}, kanaName = #{kanaName}, nickname=#{nickname}, "
       + "email = #{email}, area = #{area}, age = #{age}, sex = #{sex}, remark = #{remark}, is_deleted = #{isDeleted} WHERE id = #{id}")
   void updateStudent(Student student);
 
+  /**
+   * 受講生コース情報のコース名を更新します。
+   *
+   * @param studentCourse　受講生コース情報
+   */
   @Update("UPDATE students_courses SET course_name = #{courseName} WHERE id = #{id}")
-  void updateStudentsCourses(StudentsCourses studentsCourses);
+  void updateStudentCourse(StudentCourse studentCourse);
 }
